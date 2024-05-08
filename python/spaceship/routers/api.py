98af0f1d6e8cf20da133265fca77402eb5114338
witchkaa <victoriasabadan1@gmail.com>
2024-05-08 18:22:02 +0300

    Add numpy dependency and endpoint for alpine

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as numpy
 
 router = APIRouter()
 
@@ -6,3 +7,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+
+
+
+@router.get("/multiply-matrices")
+def multiply_matrices():
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    product = numpy.dot(matrix_a, matrix_b)
+    return {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist(),
+    }
